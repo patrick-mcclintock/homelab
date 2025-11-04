39a10c7a590da3ff8db7cd3379cb7bb04e87a527
patrick-mcclintock <68797650+patrick-mcclintock@users.noreply.github.com>
2025-11-04 12:18:35 -0500

    Create CM_Install_Roles.ps1
    Added Config Manager role installation script

---
+++ Powershell scripts/SCCM/CM_Install_Roles.ps1
@@ -0,0 +1,12 @@
+Get-Module servermanager
+Install-WindowsFeature Web-Windows-Auth
+Install-WindowsFeature Web-ISAPI-Ext
+Install-WindowsFeature Web-Metabase
+Install-WindowsFeature Web-WMI
+Install-WindowsFeature BITS
+Install-WindowsFeature RDC
+Install-WindowsFeature NET-Framework-Features -source D:\sources\sxs
+Install-WindowsFeature Web-Asp-Net
+Install-WindowsFeature Web-Asp-Net45
+Install-WindowsFeature NET-HTTP-Activation
+Install-WindowsFeature NET-Non-HTTP-Activ
